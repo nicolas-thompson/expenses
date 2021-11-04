a0a99740d01044453f71ac966945b3f26d38ef3a
Nicolas Thompson <mail@nicolasthompson.com>
2021-11-04 22:38:57 +0000

    Adds expenses filter dropdown

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const filterChangeHandler = (selectedYear) => setFilteredYear(selectedYear);
+
   return (
     <div>
       <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler} />
         {props.expenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
