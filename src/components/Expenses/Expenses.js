de1250fc2907c4077f8a35cd1fa287e19cde732e
Nicolas Thompson <mail@nicolasthompson.com>
2021-11-04 17:49:36 +0000

    Adds NewExpense component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,23 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
   return (
-    <Card className="expenses">
-      {props.expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
